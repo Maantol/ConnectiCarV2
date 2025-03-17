e9d77f2c153d58b6eee04e25c29867034f0387d2
tatuaua <tatu.niemikorpi@gmail.com>
2025-03-17 15:38:23 +0200

    add config

--- RaspberryPiScripts/mqtt_publisher.py
+++ RaspberryPiScripts/mqtt_publisher.py
@@ -1,5 +1,8 @@
 import json
 import paho.mqtt.client as mqtt
+import config as cfg
+
+config = cfg.get_config()
 
 class MQTTMessage:
     """
@@ -21,17 +24,19 @@ class MQTTMessage:
     def to_json(self):
         return json.dumps(self.__dict__)
 
-def publish_mqtt(broker: str, topic: str, message: str, port: int = 1883):
+def publish_mqtt(message: str):
     """
-    Publishes a message to a given MQTT topic.
+    Publishes a message to a configured MQTT topic.
     
-    :param broker: MQTT broker address
-    :param topic: Topic to publish to
     :param message: Message to send
-    :param port: Broker port (default is 1883)
     """
+    broker = config["mqtt"]["broker"]
+    topic = config["mqtt"]["topic"]
+    port = config["mqtt"]["port"]
+    certs_path = config["mqtt"]["certs_path"]
+    
     client = mqtt.Client()
-    client.tls_set("./certs/ca.crt")
+    client.tls_set(certs_path)
     client.tls_insecure_set(True)
     client.connect(broker, port)
     print(f"Connected to MQTT broker {broker} on port {port}")
@@ -40,6 +45,6 @@ def publish_mqtt(broker: str, topic: str, message: str, port: int = 1883):
     client.disconnect()
 
 if __name__ == "__main__":
-    publish_mqtt("connecticar-mqtt.2.rahtiapp.fi", "toyota", "Hello, MQTT!", 443)
+    publish_mqtt("Hello, MQTT!")
 
  
